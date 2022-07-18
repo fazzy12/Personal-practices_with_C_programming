e0cc69a96b420bc68724ee6846ce928b8ec9bf6d
fazzy12 <kaluifeanyi00@gmail.com>
2022-07-18 11:43:49 +0100

    calculate perimeter & area oc rect

--- rect.c
+++ rect.c
@@ -1,8 +1,10 @@
-// display parameter and are of a rectangle
+// display parameter and area of a rectangle
 
 // The code is a program that asks the user to enter two values for width and height.
 // The program then calculates the perimeter of a rectangle with those two dimensions.
 // The code is an example of how to use the printf() function in C. The code displays the perimeter and area of a   rectangle given two parameters, width and height.
+
+// AUTHOR: ifeanyi kalu
 
 #include <stdio.h>
 
