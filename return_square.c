712b548462bb750bcab0a3c914a7f388c195869f
fazzy12 <kaluifeanyi00@gmail.com>
2022-07-20 13:14:48 +0100

    convert mins to to year and days

---
+++ return_square.c
@@ -0,0 +1,13 @@
+// codewars question
+// Now you have to write a function that takes an argument and returns the square of it.
+
+#include <stdint.h>
+
+int32_t square(int32_t);
+
+int main(int sqr) 
+{
+
+    return  sqr *= sqr;
+}
+
